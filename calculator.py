35048e6760f594b743fec99da61301ca6bb1302c
Mathias Horvati Svoren <mathihs@uio.no>
2020-08-27 20:54:00 +0200

    Exercise 4 (Added functions for multiplication and cosine)

--- calculator.py
+++ calculator.py
@@ -1,2 +1,29 @@
+import math
+
 def add(x,y):
     return x + y
+
+def factorial(x):
+    ans = 1
+    for i in range(1,x+1):
+        ans *= i 
+    print(f"My implementation: {ans} | Built in: {math.factorial(x)}")
+    return ans
+
+def sin(x,N):
+    ans = 0
+    for n in range(N+1):
+        ans += ((-1)**n*x**(2*n+1))/factorial(2*n+1)
+    return ans
+
+def divide(x,y):
+    return x/y
+
+def multiply(x,y):
+    return x*y
+
+def cos(x,N):
+    ans = 0
+    for n in range(N+1):
+        ans += ((-1)**n*x**(2*n))/factorial(2*n)
+    return ans
--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,31 @@
 import calculator
+import math
 
 def test_add():
     tol = 1e-10
     assert calculator.add(0.1, 0.2) <= 0.3 + tol
     x,y = "Hello ","World"
     assert calculator.add(x,y) == "Hello World"
+
+def test_factorial():
+    assert calculator.factorial(4) == 24
+
+def test_sin():
+    tol = 1e-10
+    x = 3.14/4; N = 10
+    assert calculator.sin(x,N) <= math.sqrt(2)/2 + tol
+
+def test_divide():
+    tol = 1e-10
+    x,y = 3012387, 412381297948
+    assert calculator.divide(x,y) <= 7.30485843e-6 + tol
+
+def test_multiply():
+    tol = 1e-10
+    x,y = 4, 1.54
+    assert calculator.multiply(x,y) <= 6.16 + tol
+
+def test_cos():
+    tol = 1e-10
+    x = 3.14; N = 10
+    assert calculator.cos(x,N) <= 1 + tol
